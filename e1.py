eb4ed9421da269158e6060ad6183c7839f429c09
gacloudi <113767409+gacloudi@users.noreply.github.com>
2023-04-26 09:59:55 +0530

    Update e1.py

--- e1.py
+++ e1.py
@@ -6,6 +6,6 @@ c=getcwd()
 st.write(f.name)
 c1=c + '/' + f.name
 OP='O1.csv'
-with open(c1,"rb") as file:
+with open(f.name,"rb") as file:
                     #st.write(c)
   st.download_button(label='Download',data=file,file_name=OP,mime="text/csv") 
