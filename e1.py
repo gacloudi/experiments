f7e8f319aaa6a9ff380dcce43b17e1d94299512e
gacloudi <113767409+gacloudi@users.noreply.github.com>
2023-04-26 09:53:16 +0530

    Update e1.py

--- e1.py
+++ e1.py
@@ -3,7 +3,7 @@ from os import getcwd
 f=st.file_uploader("Upload a Invoice file")
 st.write(f)
 c=getcwd()
-st.write(c.name)
+st.write(f.name)
 # with open(c,"rb") as file:
 #                     #st.write(c)
 # st.download_button(label='Download',data=file,file_name=OP,mime="image/png") 
