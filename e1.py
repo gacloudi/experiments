7f1814058f0d9f7e4aadd68bacc6733342374f0e
gacloudi <113767409+gacloudi@users.noreply.github.com>
2023-04-26 09:54:54 +0530

    Update e1.py

--- e1.py
+++ e1.py
@@ -7,4 +7,4 @@ st.write(f.name)
 c1=c + '/' + f.name
 with open(c1,"rb") as file:
                     #st.write(c)
-st.download_button(label='Download',data=file,file_name=OP,mime="xlsx") 
+  st.download_button(label='Download',data=file,file_name=OP,mime="xlsx") 
