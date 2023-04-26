6f374c4cd559652255e8e14df99b744a23580c6a
gacloudi <113767409+gacloudi@users.noreply.github.com>
2023-04-26 10:25:12 +0530

    Update e2.py

--- e2.py
+++ e2.py
@@ -1,5 +1,6 @@
 import streamlit as st
 OP1='O1.csv'
-with open("OP.csv","rb") as file:
+in1='/app/experiments/OP.csv"
+with open(in1,"rb") as file:
                     #st.write(c)
   st.download_button(label='Download',data=file,file_name=OP1,mime="text/csv")
