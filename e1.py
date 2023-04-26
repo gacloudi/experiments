12d904b9c9cfd5c0d2033c36e8afb990142386dd
gacloudi <113767409+gacloudi@users.noreply.github.com>
2023-04-26 10:27:13 +0530

    Update e1.py

--- e1.py
+++ e1.py
@@ -7,7 +7,7 @@ st.write(c)
 def save_uploadedfile(uploadedfile):
      with open(os.path.join(c,uploadedfile.name),"wb") as f:
          f.write(uploadedfile.getbuffer())
-     return st.success("Saved File:{} to tempDir".format(uploadedfile.name))
+     return st.success("Saved File:{} to {}".format(uploadedfile.name,c))
 #f=st.file_uploader("Upload a Invoice file")
 datafile = st.file_uploader("Upload CSV",type=['csv'])
 if datafile is not None:
