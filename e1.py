56b8f515750d8cee5cda5a9bedd62afd983701a2
gacloudi <113767409+gacloudi@users.noreply.github.com>
2023-04-26 10:10:24 +0530

    Update e1.py

--- e1.py
+++ e1.py
@@ -2,8 +2,7 @@ import streamlit as st
 import os
 from os import getcwd
 import pandas as pd
-with open(os.path.join("tempDir",uploadedfile.name),"wb") as f:
-      f.write(uploadedfile.getbuffer())
+
 
 def save_uploadedfile(uploadedfile):
      with open(os.path.join("tempDir",uploadedfile.name),"wb") as f:
@@ -21,6 +20,6 @@ if datafile is not None:
 # st.write(f.name)
 # c1=c + '/' + f.name
 # OP='O1.csv'
-with open(datafile.name,"rb") as file:
-                    #st.write(c)
-  st.download_button(label='Download',data=file,file_name=OP,mime="text/csv") 
+# with open(datafile.name,"rb") as file:
+#                     #st.write(c)
+#   st.download_button(label='Download',data=file,file_name=OP,mime="text/csv") 
