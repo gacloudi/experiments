9ae7ddf7446a0a93bb2190562dae8c670a4188fe
gacloudi <113767409+gacloudi@users.noreply.github.com>
2023-04-26 10:15:39 +0530

    Update e1.py

--- e1.py
+++ e1.py
@@ -19,7 +19,7 @@ if datafile is not None:
 # c=getcwd()
 # st.write(f.name)
 # c1=c + '/' + f.name
-# OP='O1.csv'
+OP='O1.csv'
 with open(datafile.name,"rb") as file:
                     #st.write(c)
   st.download_button(label='Download',data=file,file_name=OP,mime="text/csv") 
