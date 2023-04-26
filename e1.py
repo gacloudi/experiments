3d396683bedc75b7ea0f1e497534651bed162885
gacloudi <113767409+gacloudi@users.noreply.github.com>
2023-04-26 10:03:27 +0530

    Update e1.py

--- e1.py
+++ e1.py
@@ -1,11 +1,17 @@
 import streamlit as st
 from os import getcwd
-f=st.file_uploader("Upload a Invoice file")
-st.write(f)
-c=getcwd()
-st.write(f.name)
-c1=c + '/' + f.name
-OP='O1.csv'
-with open(f.name,"rb") as file:
+#f=st.file_uploader("Upload a Invoice file")
+datafile = st.file_uploader("Upload CSV",type=['csv'])
+if datafile is not None:
+   file_details = {"FileName":datafile.name,"FileType":datafile.type}
+   df  = pd.read_csv(datafile)
+   st.dataframe(df)
+   save_uploadedfile(datafile)
+# st.write(f)
+# c=getcwd()
+# st.write(f.name)
+# c1=c + '/' + f.name
+# OP='O1.csv'
+with open(datafile,"rb") as file:
                     #st.write(c)
   st.download_button(label='Download',data=file,file_name=OP,mime="text/csv") 
